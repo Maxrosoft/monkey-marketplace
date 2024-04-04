62aa3064b7ba96a54708570f72461e87ec766713
maxrosoft <maxrosoft.me@gmail.com>
2024-04-04 11:49:24 +0200

    added .env and README.md

---
+++ README.md
@@ -0,0 +1,33 @@
+# E-commerce Website with Node.js, Express, and PostgreSQL (Monkey Marketplace)
+
+## Overview
+This is a simple e-commerce website that allows users to browse products. It uses Node.js, Express, and PosrgreSQL for the backend, and Bootstrap for the frontend.
+
+## Getting Started
+
+1. Clone this repository to your local machine: `git clone https://github.com/Maxrosoft/monkey-marketplace.git`
+2. Install required packages: `npm install`
+3. Start the server: `npm start`
+4. Open a web browser and navigate to `http://localhost:3000/`
+
+## Features
+
+* Admin dashboard for product management
+* Product catalog
+* Shopping cart
+
+## Screenshots
+
+### Home Page
+![Home Page](screenshots/home-page.png)
+
+### Product Catalog
+![Product Catalog](screenshots/product-catalog.png)
+
+### Shopping Cart
+![Shopping Cart](screenshots/shopping-cart.png)
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
--- models/product.js
+++ models/product.js
@@ -1,91 +1,97 @@
-const pg = require('pg');
+const pg = require("pg");
+const env = require("dotenv");
+
+env.config();
 
 const db = new pg.Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'shop',
-  password: '06072007Max',
-  port: 5432,
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
 });
 
 db.connect();
 
 const cart = [];
 
-module.exports = {Product: class Product {
-  constructor(title, price, description) {
-    this.title = title;
-    this.price = price;
-    this.description = description;
-  }
+module.exports = {
+  Product: class Product {
+    constructor(title, price, description) {
+      this.title = title;
+      this.price = price;
+      this.description = description;
+    }
 
-  save() {
-    db.query(
-      'INSERT INTO products (title, price, description) VALUES ($1, $2, $3)',
-      [this.title, this.price, this.description]
-    ).catch(err => console.error(err));
-  }
+    save() {
+      db.query(
+        "INSERT INTO products (title, price, description) VALUES ($1, $2, $3)",
+        [this.title, this.price, this.description]
+      ).catch((err) => console.error(err));
+    }
 
-  static edit(id, title, price, description) {
-    db.query(
-      'UPDATE products SET title = $1, price = $2, description = $3 WHERE id = $4',
-      [title, price, description, id]
-    ).catch(err => console.error(err));
-  }
+    static edit(id, title, price, description) {
+      db.query(
+        "UPDATE products SET title = $1, price = $2, description = $3 WHERE id = $4",
+        [title, price, description, id]
+      ).catch((err) => console.error(err));
+    }
 
-  static delete(id) {
-    db.query('DELETE FROM products WHERE id = $1', [id])
-      .catch(err => console.error(err));
-  }
+    static delete(id) {
+      db.query("DELETE FROM products WHERE id = $1", [id]).catch((err) =>
+        console.error(err)
+      );
+    }
 
-  static fetchAll() {
-    return new Promise((resolve, reject) => {
-      db.query('SELECT * FROM products', (err, res) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(res.rows);
-        }
+    static fetchAll() {
+      return new Promise((resolve, reject) => {
+        db.query("SELECT * FROM products", (err, res) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(res.rows);
+          }
+        });
       });
-    });
-  }
+    }
 
-  static findById(id) {
-    return new Promise((resolve, reject) => {
-      db.query('SELECT * FROM products WHERE id = $1', [id], (err, res) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(res.rows[0]);
-        }
+    static findById(id) {
+      return new Promise((resolve, reject) => {
+        db.query("SELECT * FROM products WHERE id = $1", [id], (err, res) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(res.rows[0]);
+          }
+        });
       });
-    });
-  }
+    }
 
-  static addToCart(id) {
-    return new Promise((resolve, reject) => {
-      db.query('SELECT * FROM products WHERE id = $1', [id], (err, res) => {
-        if (err) {
-          reject(err);
-        } else {
-          cart.push(res.rows[0]);
-          resolve();
-        }
+    static addToCart(id) {
+      return new Promise((resolve, reject) => {
+        db.query("SELECT * FROM products WHERE id = $1", [id], (err, res) => {
+          if (err) {
+            reject(err);
+          } else {
+            cart.push(res.rows[0]);
+            resolve();
+          }
+        });
       });
-    });
-  }
+    }
 
-  static deleteFromCart(id) {
-    return new Promise((resolve, reject) => {
-      db.query('SELECT * FROM products WHERE id = $1', [id], (err, res) => {
-        if (err) {
-          reject(err);
-        } else {
-          cart.splice(cart.indexOf(res.rows[0]), 1);
-          resolve();
-        }
+    static deleteFromCart(id) {
+      return new Promise((resolve, reject) => {
+        db.query("SELECT * FROM products WHERE id = $1", [id], (err, res) => {
+          if (err) {
+            reject(err);
+          } else {
+            cart.splice(cart.indexOf(res.rows[0]), 1);
+            resolve();
+          }
+        });
       });
-    });
-  }
-  
-}, cart: cart};
+    }
+  },
+  cart: cart,
+};
